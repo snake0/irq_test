4d8a24402744390bdfc2816be029957507d5d575
snake0 <1260865816@qq.com>
2020-02-23 11:37:18 +0800

    add mem_flush

--- utils.h
+++ utils.h
@@ -39,4 +39,27 @@ static unsigned long long inline rand_gen(rand_t *rand) {
   return t + s;
 }
 
+static inline void mem_flush(const void *p, unsigned int allocation_size)
+{
+  const size_t cache_line = 64;
+  const char *cp = (const char *)p;
+  size_t i = 0;
+
+  if (!p || allocation_size <= 0)
+    return;
+
+  for (i = 0; i < allocation_size; i += cache_line)
+  {
+    asm volatile("clflush (%0)\n\t"
+                 :
+                 : "r"(&cp[i])
+                 : "memory");
+  }
+
+  asm volatile("sfence\n\t"
+               :
+               :
+               : "memory");
+}
+
 #endif
