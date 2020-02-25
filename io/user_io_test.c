d53f0a72c381a3bfc0e297a84744887d570cd08c
Yu <2314310541@qq.com>
2020-02-25 18:52:02 +0800

    change write to pwrite

--- io/user_io_test.c
+++ io/user_io_test.c
@@ -23,9 +23,11 @@ void test_io()
   char read_buf[BLOCK_SIZE];
   char write_buf[BLOCK_SIZE];
   uint64_t begin, end;
+  off_t pos;
   memset(write_buf, 'c', BLOCK_SIZE);
 
-  if (bind_cpu){
+  if (bind_cpu)
+  {
     if (pthread_setaffinity_np(pthread_self(), sizeof(mask), &mask) < 0)
     {
       printf("Set thread affinity error!\n");
@@ -35,36 +37,36 @@ void test_io()
   // test read
   for (int i = 0; i < 10; ++i)
   {
-    if((fd=open(filename, O_RDONLY | O_SYNC)) == -1)
+    if ((fd = open(filename, O_RDONLY | O_SYNC)) == -1)
     {
       printf("File open error!\n");
     }
     begin = rdtscp();
-    for(int j=0;j<100;++j)
-
-    ret = pread(fd, read_buf, BLOCK_SIZE,0);
+    for (int j = 0; j < 100; ++j)
+      ret = pread(fd, read_buf, BLOCK_SIZE, 0);
     end = rdtscp();
-    if (ret != BLOCK_SIZE)
-      printf("Not expected size %d!\n", ret);
     printf("[READ BIND=%d] %lu\n", bind_cpu, end - begin);
     close(fd);
   }
 
+  // test sequential write
+  pos = 0;
   for (int i = 0; i < 10; ++i)
   {
     // Open file
-    if((fd=open("./temp.txt", O_WRONLY | O_TRUNC| O_SYNC, S_IRWXG)) == -1)
+    if ((fd = open("./temp.txt", O_WRONLY | O_TRUNC | O_SYNC, S_IRWXG)) == -1)
     {
       printf("File open error!\n");
     }
 
     begin = rdtscp();
-    for(int j=0;j<100;++j)
-    ret = write(fd, write_buf, BLOCK_SIZE);
+    for (int j = 0; j < 100; ++j)
+    {
+      ret = pwrite(fd, write_buf, BLOCK_SIZE, pos);
+      pos + BLOCK_SIZE;
+    }
     end = rdtscp();
 
-    if (ret != BLOCK_SIZE)
-      printf("Not expected size %d!\n", ret);
     printf("[WRITE BIND=%d] %lu\n", bind_cpu, end - begin);
     close(fd);
   }
@@ -83,7 +85,7 @@ int main(int argc, char **argv)
   {
     cpu_id = atoi(argv[3]);
     CPU_SET(cpu_id, &mask);
-     if ((ret = pthread_create(&pid, NULL, (void *)test_io, NULL)) != 0)
+    if ((ret = pthread_create(&pid, NULL, (void *)test_io, NULL)) != 0)
     {
       printf("Create thread error!\n");
     }
